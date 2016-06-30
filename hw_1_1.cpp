2402fa75ed4abd77a1ee71ba08688f1900d42d44
My Name <my_email@mail.ru>
2016-06-30 21:13:58 +0300

    Added multiplication of vars

--- hw_1_1.cpp
+++ hw_1_1.cpp
@@ -5,8 +5,11 @@ int main(int argc, char** argv) {
     int a = 5;
     int b = 7;
     int c = a + b;
+    int d = a * b;
 
     printf("%i + %i = %i\n", a, b, c);
+
+    printf("%i * %i = %i\n", a, b, d);
 
     return 0;
 }
